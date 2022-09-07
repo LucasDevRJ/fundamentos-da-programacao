ec3aa3a57d6d27acc8b8ba8d10120b533c1acc4a
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-07 19:34:53 -0300

    Exemplos de variáveis em JS

---
+++ 3-variaveis_e_tipos_de_dados/script.js
@@ -0,0 +1,17 @@
+var idade = 34;
+var email = "eu@a.com";
+var data = "01/01/19";
+var ano;
+
+ano = 2019;
+
+console.log(idade);
+console.log(email);
+console.log(data);
+console.log(ano);
+
+idade = 39;
+email = "abc@d.com";
+
+console.log(idade);
+console.log(email);
