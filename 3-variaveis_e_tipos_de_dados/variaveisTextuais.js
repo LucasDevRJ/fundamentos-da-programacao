6ea34ac06f7aaf459c186c4000135f53d8d92f6f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-07 21:03:56 -0300

    Mais exemplos de variáveis String

--- 3-variaveis_e_tipos_de_dados/variaveisTextuais.js
+++ 3-variaveis_e_tipos_de_dados/variaveisTextuais.js
@@ -7,3 +7,16 @@ console.log(endereco);
 endereco = 'Praça da Sé, 45 Apt#5';
 
 console.log(endereco);
+
+//Barra inversa para indicar que as aspas simples são textos
+var frase = 'Não misture o uso de aspas duplas " e aspas simples \' dentro do seu texto';
+
+console.log(frase);
+
+//Propriedades da String
+var frase2 = "Olá Mundo!";
+
+console.log(frase2);
+console.log(frase2.length); //pega o número total de caracteres da variável
+console.log(frase2.search("Mundo")); //pesquisa se existe a palavra referenciada e retorna a posição que ela ocupe
+console.log(frase2.toUpperCase()); //converte a variável para maiúscula
