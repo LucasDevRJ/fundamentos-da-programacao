eb46d4dc9c74a3ab6c6b148f1c331c1f5e8df5f7
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-07 21:22:04 -0300

    Exemplos de operadores e atribuições de valor

---
+++ 3-variaveis_e_tipos_de_dados/operadores.js
@@ -0,0 +1,64 @@
+//O símbolo de igual (=) atribui valor a variável desejada
+var lucro = 500;
+
+console.log("Lucro = " + lucro);
+
+//Operações aritméticas
+
+var a = 100;
+var b = 50;
+var resultado = a + b; //soma
+
+console.log(a + " + " + b + " = " + resultado);
+
+resultado = a - b; //subtração
+
+console.log(a + " - " + b + " = " + resultado);
+
+resultado = a * b; //subtração
+
+console.log(a + " x " + b + " = " + resultado);
+
+resultado = a / b; //subtração
+
+console.log(a + " / " + b + " = " + resultado);
+
+var conta = 5 + 5 * 10; //a multiplicação será feita primeiro
+
+console.log(conta);
+
+conta = (5 + 5) * 10; //a soma será feita primeiro
+
+console.log(conta);
+
+var pontos = 55
+pontos = pontos + 10;
+
+console.log(pontos);
+
+//outra forma de se fazer o mesmo
+pontos += 10;
+
+console.log(pontos);
+
+pontos -= 10;
+
+console.log(pontos);
+
+pontos *= 10;
+
+console.log(pontos);
+
+pontos /= 10;
+
+console.log(pontos);
+
+//outra forma de incrementar somente um
+pontos++;
+
+console.log(pontos);
+
+//também podemos decrementar
+pontos--;
+
+console.log(pontos);
